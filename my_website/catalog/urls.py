ddd32f2e41012c6f170faeedf42d239ba1ad947a
Shopping-Yuan <74536262+Shopping-Yuan@users.noreply.github.com>
2023-08-14 00:16:32 +0800

    Add files via upload

--- my_website/catalog/urls.py
+++ my_website/catalog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("index/",views.my_index,name="index_url"),
     path("add_data/",views.add_data,name="add_data_url"),
+    path("renew_data/",views.renew_data,name="renew_data_url"),
     path('search/', views.search),
 ]
--- my_website/catalog/views.py
+++ my_website/catalog/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+#from django.http import HttpResponse, HttpResponseRedirect
 from .models import all_card,CardForm
 def my_index(request):
     cards = all_card.objects.all()
     columns = {
-        "contend" : cards
+        "content" : cards
  }
     return  render(request, 'index.html', context=columns)
 
@@ -16,11 +16,27 @@ def add_data(request):
         new_card = CardForm(request.POST)
         if new_card.is_valid():
             new_card.save()
-        return HttpResponseRedirect('http://127.0.0.1:8000/catalog/index/')
-    else:
-        new_card = CardForm()
-    contend ={'form':CardForm()}
-    return  render(request, 'add_data.html',contend)
+        return redirect('index_url')
+    content ={'form':CardForm()}
+    return  render(request, 'add_data.html',content)
+def renew_data(request):
+    content = {}
+    if request.method == 'POST':
+        if request.POST.get("action") == "search":
+            s_id = request.POST['search_id']
+            try :
+                renew_card = all_card.objects.get(card_ID=s_id)
+                content["old_card"] = renew_card
+            except:
+                return redirect('renew_data_url')
+        if request.POST.get("action") == "renew":
+                print(request.method)
+                new_card = all_card.objects.get(card_ID=request.POST.get("new_ID"))
+                new_card.card_name = request.POST.get("new_name")
+                new_card.card_type = request.POST.get("new_type")
+                new_card.save()
+                content['card'] = new_card
+    return  render(request, 'renew_data.html',content)
 def search(request):
     ctx ={}
     if request.POST:
